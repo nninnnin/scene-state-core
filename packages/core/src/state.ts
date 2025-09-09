d362edffd290b47a8c7c396eced879208a011fa6
이동규 (Justin) <nninnnin7@gmail.com>
2025-09-09 20:01:22 +0900

    Add `removeEntity` function and tests

--- packages/core/src/state.ts
+++ packages/core/src/state.ts
@@ -12,6 +12,20 @@ export function createEmptyState(): State {
   return { entities: {} };
 }
 
+export function removeEntity(state: State, id: EntityId): State {
+  const hasEntityId = Boolean(state.entities[id]);
+  if (!hasEntityId) return state;
+
+  const updatedEntities = Object.fromEntries(
+    Object.entries(state.entities).filter(([entityId]) => entityId !== id),
+  );
+
+  return {
+    ...state,
+    entities: updatedEntities,
+  };
+}
+
 export function addEntity(state: State, id: EntityId, name: string): State {
   return {
     ...state,
