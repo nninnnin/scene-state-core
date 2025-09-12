8105852d8e6d54723888dcc24a7aa2cfff6a4e98
이동규 (Justin) <nninnnin7@gmail.com>
2025-09-12 21:23:51 +0900

    Add `SetTransformCommand` command class

---
+++ packages/core/src/command/commands/SetTransformCommand.ts
@@ -0,0 +1,41 @@
+import { Transform } from "../../transform/types";
+import { State } from "../../state";
+import { Command } from "../types";
+import { setTransform } from "../../transform";
+
+export class SetTrasnformCommand
+  implements Command
+{
+  readonly type = "SetTransform";
+  private prev?: Transform;
+
+  constructor(
+    private entityId: string,
+    private patch: Partial<Transform>,
+  ) {}
+
+  execute(state: State): State {
+    this.prev =
+      state.components.transform[
+        this.entityId
+      ];
+
+    return setTransform(
+      state,
+      this.entityId,
+      this.patch,
+    );
+  }
+
+  undo(state: State): State {
+    if (this.prev) {
+      return setTransform(
+        state,
+        this.entityId,
+        this.prev,
+      );
+    }
+
+    return state;
+  }
+}
