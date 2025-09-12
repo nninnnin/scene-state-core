190b6d47d93617453d2661742324047b6dc50660
이동규 (Justin) <nninnnin7@gmail.com>
2025-09-12 21:04:47 +0900

    Add `RemoveEntityCommand` command class

---
+++ packages/core/src/command/commands/RemoveEntityCommand.ts
@@ -0,0 +1,57 @@
+import {
+  addEntity,
+  removeEntity,
+  State,
+} from "../../state";
+import { setTransform } from "../../transform";
+import { Transform } from "../../transform/types";
+import { Command } from "../types";
+
+export class RemoveEntityCommand
+  implements Command
+{
+  readonly type = "RemoveEntity";
+  private prevName?: string;
+  private prevTransform?: Transform;
+
+  constructor(
+    private entityId: string,
+  ) {}
+
+  execute(state: State): State {
+    const entity =
+      state.entities[this.entityId];
+
+    this.prevName = entity?.name;
+    this.prevTransform =
+      state.components.transform[
+        this.entityId
+      ];
+
+    return removeEntity(
+      state,
+      this.entityId,
+    );
+  }
+
+  undo(state: State) {
+    if (this.prevName == null)
+      return state;
+
+    let next = addEntity(
+      state,
+      this.entityId,
+      this.prevName,
+    );
+
+    if (this.prevTransform) {
+      next = setTransform(
+        next,
+        this.entityId,
+        this.prevTransform,
+      );
+    }
+
+    return next;
+  }
+}
