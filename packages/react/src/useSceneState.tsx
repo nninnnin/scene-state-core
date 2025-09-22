9e0dea471af4ed258f9778996fed941a489c4a7a
이동규 (Justin) <nninnnin7@gmail.com>
2025-09-23 01:36:01 +0900

    `@ssc/react`: Add selector/equals signature to `useSceneState` (types only)

--- packages/react/src/useSceneState.tsx
+++ packages/react/src/useSceneState.tsx
@@ -1,8 +1,22 @@
 import { useState } from "react";
-import { getState } from "@ssc/core";
+import { getState, State } from "@ssc/core";
 
-export const useSceneState = () => {
-  const [sceneState, _] = useState(() => getState());
+// overloads
+export function useSceneState(): { sceneState: State };
+export function useSceneState<T>(
+  selector: () => T,
+  equals?: (a: T, b: T) => boolean,
+): { sceneState: State };
+
+// implementation
+export function useSceneState<T = State>(
+  selector?: (state: State) => T,
+  _equals?: (a: T, b: T) => boolean,
+) {
+  const [sceneState] = useState<T>(() => {
+    const state = getState();
+    return selector ? selector(state) : (state as T);
+  });
 
   return { sceneState };
-};
+}
