d1b1f1ceb2a8a5b9991cdc537210c4f69f2871dc
이동규 (Justin) <nninnnin7@gmail.com>
2025-09-27 19:45:52 +0900

    `playground`: Add tailwindcss

--- apps/playground/src/main.tsx
+++ apps/playground/src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { SceneStateProvider } from '@ssc/react';
+import { createEmptyState } from '@ssc/core';
 
 import App from './App.tsx';
-import { createEmptyState } from '@ssc/core';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
