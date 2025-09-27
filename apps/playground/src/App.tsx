d794a8ee4406ad71b34c6bcf52cedd3b70c1ebf9
이동규 (Justin) <nninnnin7@gmail.com>
2025-09-27 19:55:06 +0900

    `playground`: Add `add entity` button

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -1,19 +1,27 @@
+import clsx from 'clsx';
+import { useCommand, useSceneState } from '@ssc/react';
 import { Canvas } from '@react-three/fiber';
-
-import { version as coreVersion } from '@ssc/core';
 import { OrbitControls } from '@react-three/drei';
-import { useSceneState } from '@ssc/react';
+import { AddEntityCommand, version as coreVersion } from '@ssc/core';
 
 function App() {
   const { sceneState } = useSceneState();
 
   console.log('default scene state: ', sceneState);
 
-  return (
-    <>
-      <div>core: {coreVersion()}</div>
+  const { dispatch } = useCommand();
 
-      <Canvas style={{ height: 400 }}>
+  const handleAddEntityClick = () => {
+    dispatch(new AddEntityCommand('my-id', 'Justin'));
+  };
+
+  return (
+    <div className="flex flex-col h-[100dvh]">
+      <div id="navigation-bar" className="bg-gray-100 p-3">
+        core: {coreVersion()}
+      </div>
+
+      <Canvas className="flex-1">
         <mesh>
           <boxGeometry />
           <meshStandardMaterial />
@@ -22,7 +30,16 @@ function App() {
         <ambientLight />
         <OrbitControls />
       </Canvas>
-    </>
+
+      <div id="button-container" className="bg-gray-100 p-5">
+        <button
+          className={clsx('bg-amber-200 p-3 px-2 rounded-xl')}
+          onClick={handleAddEntityClick}
+        >
+          엔티티 추가
+        </button>
+      </div>
+    </div>
   );
 }
 
